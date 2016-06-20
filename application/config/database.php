c3ad6c3950ae341899f558636363bdd674b3d4b7
Dark Demon <darkromanovich@gmail.com>
2016-06-20 04:53:25 -0400

    db

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'master';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -79,6 +79,29 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'givicool',
 	'database' => 'car_rental',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+
+$db['master'] = array(
+	'dsn'	=> '',
+	'hostname' => 'ntec.mysql.ukraine.com.ua',
+	'username' => 'ntec_rental',
+	'password' => 'ydwehvtm',
+	'database' => 'ntec_rental',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
