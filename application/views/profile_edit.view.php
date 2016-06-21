bd9fd28e1a2119d07ebf3fab6a342533821e3142
Dark Demon <darkromanovich@gmail.com>
2016-06-20 21:51:25 -0400

    register

--- application/views/profile_edit.view.php
+++ application/views/profile_edit.view.php
@@ -1,13 +1,11 @@
-
-
 <div class="container">
 <div class="row">
     <div class="col-sm-3 col-md-2 sidebar">
-      	<ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview</a></li>
-            <li><a href="#">Change Password</a></li>
-            <li><a href="#">My Orders</a></li>
-        </ul>
+ 		<div class="list-group">
+  			<a href="#" class="list-group-item active">Info</a>
+  			<a href="#" class="list-group-item">Change Password</a>
+  			<a href="#" class="list-group-item">My Orders</a>
+		</div>
     </div>
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Info:</h1>
