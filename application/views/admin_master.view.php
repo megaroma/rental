53302ded4acc88705ad97174f0c5695c81e24a2e
Dark Demon <darkromanovich@gmail.com>
2016-06-21 08:34:44 -0400

    admin

--- application/views/admin_master.view.php
+++ application/views/admin_master.view.php
@@ -73,6 +73,51 @@
   </head>
 
   <body>
+
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://<?php echo base_url(); ?>admin">Admin</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="http://<?php echo base_url(); ?>admin">Home</a></li>
+
+              <li class="dropdown" >
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admin <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="">Users</a></li>
+                  <li><a href="">Roles</a></li>
+                </ul>
+              </li>
+            </ul>
+
+ 
+            <ul class="nav navbar-nav navbar-right">
+              <?php if($this->auth->check()): ?>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $this->auth->get_user()->full_name; ?> <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="http://<?php echo base_url(); ?>">Web Site</a></li>
+                  <li><a href="http://<?php echo base_url(); ?>home/logout">Logout</a></li>
+                </ul>
+              </li>  
+              <?php endif; ?>         
+            </ul>
+
+
+
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+
 <?php echo $content; ?>
 
     <script src="<?php echo asset_url('js/ie10-viewport-bug-workaround.js');?>"></script>
