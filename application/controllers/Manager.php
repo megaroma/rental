474614c14fd4486df75b1c7dcfce1142db425db0
Dark Demon <darkromanovich@gmail.com>
2016-06-21 19:01:31 -0400

    manager

--- application/controllers/Manager.php
+++ application/controllers/Manager.php
@@ -37,6 +37,7 @@ class Manager extends CI_Controller {
 		$crud = $this->grocery_crud;
 		$crud->set_table('cars');
 		$crud->set_crud_url_path("http://".base_url().'manager/cars');
+		$crud->set_field_upload('photo','assets/imgs/cars');
 		admin_view($this, 'crud' , $data);
 
 	}
