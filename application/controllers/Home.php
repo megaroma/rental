f8207b75ac87ad95f48d63eaaaa348d7a56f320b
Dark Demon <darkromanovich@gmail.com>
2016-06-20 21:17:06 -0400

    register

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -76,7 +76,7 @@ class Home extends CI_Controller {
 
 		$user['full_name'] = input_post("full_name","");
 		$user['email'] = input_post("email","");
-		$user['password'] = trim(input_post("pasword",""));
+		$user['password'] = trim(input_post("password",""));
 		$user['driver_licence'] = input_post("driver_licence","");
 		$user['age'] = input_post("age","");
 		$user['gender'] = input_post("gender","");
@@ -88,7 +88,7 @@ class Home extends CI_Controller {
 			if ($this->form_validation->run() == FALSE) {
 				$data['error'] = true;
 			} else {
-				//$user['password'] = md5($user['password']);
+				$user['password'] = md5($user['password']);
 				$user['created_at'] = date("Y-m-d H:i:s",strtotime("NOW"));
 				$user['updated_at'] = date("Y-m-d H:i:s",strtotime("NOW"));
 				$user['role_id'] = "3";
