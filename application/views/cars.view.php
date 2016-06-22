e9ff8e499b6779b766d235220a43c678be0b8cad
Dark Demon <darkromanovich@gmail.com>
2016-06-21 22:25:52 -0400

    manager

--- application/views/cars.view.php
+++ application/views/cars.view.php
@@ -1,7 +1,12 @@
-<?php foreach($cars as $car): ?>
+
 <div class="container">
 <h1>Cars</h1>
 	<div class="row">
+		<?php 
+		$i=0;
+		foreach($cars as $car): ?>
+
+		<?php $i++; ?>
 		<div class="col-md-3">
 			<img src="assets/imgs/cars/<?php echo $car->photo; ?>" alt="" width="270" height="190">
 			<h4><?php echo $car->brand; ?> <?php echo $car->model; ?></h4>
@@ -12,12 +17,12 @@
 			</ul>
 			<a class="btn btn-large car_button" href="#"><span>Details</span></a>
 		</div>
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-3">
-		</div>
+		<?php 
+		if($i == 4) {
+			echo "</div>";
+			echo "<div class=\"row\">";
+			$i = 0;
+		}
+		endforeach; ?>
 	</div>
 </div>
-<?php endforeach; ?>
