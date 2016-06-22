690ec3421bff4502516d17461248dcbd36e6e653
Dark Demon <darkromanovich@gmail.com>
2016-06-22 00:19:26 -0400

    manager

--- application/views/master.view.php
+++ application/views/master.view.php
@@ -164,7 +164,6 @@
     <script src="assets/js/jquery.appear.min.js"></script>
     <script src="assets/js/twitterFetcher.min.js"></script>
 	<script src="contact/jqBootstrapValidation.js"></script>
-	<script src="contact/contact_me.js"></script>
     <script src="assets/js/script.js"></script> 
     <script src="<?php echo asset_url('grocery_crud/js/jquery_plugins/jquery.form.min.js');?>"></script>
     <script src="<?php echo asset_url('grocery_crud/js/jquery_plugins/ui/jquery-ui-1.10.3.custom.min.js');?>"></script>
