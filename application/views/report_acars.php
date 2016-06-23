85468a1135fd20bbed4b2ff6a3ddce2dd94dbad1
Dark Demon <darkromanovich@gmail.com>
2016-06-22 22:59:50 -0400

    fix

--- application/views/report_acars.php
+++ application/views/report_acars.php
@@ -1,5 +1,24 @@
 
 <h3>Available Cars:</h3>
 
+<p>
+<table class="table table-striped table-bordered table-hove">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Car</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach($available as $car): ?>
+			<tr>
+				<td><?php echo $car->id; ?></td>
+				<td><?php echo $car->brand." ".$car->model; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+</p>
+
 
 <?php echo count($available); ?>
