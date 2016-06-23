a26bed81328c6ab862af6875ebf179909df45c2b
Dark Demon <darkromanovich@gmail.com>
2016-06-22 23:03:48 -0400

    fix

--- application/views/report_acars.php
+++ application/views/report_acars.php
@@ -5,7 +5,7 @@
 <table class="table table-striped table-bordered table-hove">
 	<thead>
 		<tr>
-			<th>ID</th>
+			<th>Car id</th>
 			<th>Car</th>
 		</tr>
 	</thead>
@@ -20,5 +20,27 @@
 </table>
 </p>
 
+<h3>Booked Cars:</h3>
 
-<?php echo count($available); ?>
+<p>
+<table class="table table-striped table-bordered table-hove">
+	<thead>
+		<tr>
+			<th>Car id</th>
+			<th>Car</th>
+			<th>User</th>
+			<th>Days</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach($booked as $car): ?>
+			<tr>
+				<td><?php echo $car->id; ?></td>
+				<td><?php echo $car->brand." ".$car->model; ?></td>
+				<td><?php echo $car->user; ?></td>
+				<td><?php echo $car->start_rent_time." - ".$car->end_rent_time; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+</p>
